45ae79296e7d8690d39741b0a2923f3eb01646be
GRMARTM <120740684+GRMARTM@users.noreply.github.com>
2023-02-13 18:00:14 +0300

    Домашнее задание к 8 уроку

---
+++ HW/Task_56/Program.cs
@@ -0,0 +1,75 @@
+﻿/* 
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+int[,] arr = new int[5, 5];
+FillArray(arr);
+Console.WriteLine("Заданный массив: ");
+PrintArray(arr);
+Console.WriteLine();
+MinSumInRowFinder(arr);
+
+
+
+// Функция заполнения массива рандомно числами от 1 до 9
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+// Функция вывода двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Функция вывода номера строки (не индекса) с наименьшей суммой элементов 
+void MinSumInRowFinder(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            sumRow += array[i, j];
+            
+        }
+    Console.WriteLine($"Сумма значений в сроке {i+1}  = {sumRow}");
+        
+       
+
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка обладает наименьшей суммой элементов");
+}
---
+++ HW/Task_60/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)*/
+
+int[,,] array = new int[2, 2, 2];
+FillArray(array);
+PrintIndex(array);
+
+// Функция заполнения 3D массива не повторяющимеся числами
+void FillArray(int[,,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int z = 0; z < arr.GetLength(2); z++)
+            {
+                arr[z, i, j] += count;
+                count += 1;
+            }
+        }
+    }
+}
+// Функция вывода индекса элементов 3D массива
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int z = 0; z < arr.GetLength(2); z++)
+            {
+                Console.Write($"{arr[i, j, z]}({i},{j},{z}) ");
+            }
+        }
+    }
+}
